60b65e3bdac3b240498e13b66805d4255ce5c2e6
pannashohel <pannashohel2013@gmail.com>
2022-09-24 09:10:37 +0600

    Sum files upload..

---
+++ Add.java
@@ -0,0 +1,10 @@
+class Add{
+    public static void main(String arg[]){
+        System.out.println("Hi !! Welcome to UTC");
+        Add ad=new Add();
+        ad.show();
+    }
+    void show(){
+        System.out.println("This is show");
+    }
+}
---
+++ sum.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int a=3,b=4;
+    a=a+b;
+    printf("add is=%d",a);
+}
